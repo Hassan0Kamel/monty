f16009ea2a9bcec341ea60207b41a6d7414697e1
Hassan0Kamel <103977827+Hassan0Kamel@users.noreply.github.com>
2023-05-24 21:58:11 +0300

    Create executex.c

---
+++ executex.c
@@ -0,0 +1,54 @@
+#include "monty.h"
+
+/**
+* execute - execode
+* @stack: head st - stack
+* @counter: linnter
+* @file: poin file
+* @content: linnt
+* Return: nourn
+*/
+
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+{
+	instruction_t opst[] = {
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop", f_pop},
+				{"swap", f_swap},
+				{"add", f_add},
+				{"nop", f_nop},
+				{"sub", f_sub},
+				{"div", f_div},
+				{"mul", f_mul},
+				{"mod", f_mod},
+				{"pchar", f_pchar},
+				{"pstr", f_pstr},
+				{"rotl", f_rotl},
+				{"rotr", f_rotr},
+				{"queue", f_queue},
+				{"stack", f_stack},
+				{NULL, NULL}
+				};
+	unsigned int s = 0;
+	char *opp;
+
+	opp = strtok(content, " \n\t");
+	if (opp && opp[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && opp)
+	{
+		if (strcmp(opp, opst[i].opcode) == 0)
+		{	opst[s].f(stack, counter);
+			return (0);
+		}
+		s++;
+	}
+	if (opp && opst[s].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, opp);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
+	return (1);
+}
