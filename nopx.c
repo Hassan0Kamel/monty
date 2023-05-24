d208e2c1e668d00258440888d5c136c56bf2f64e
Hassan0Kamel <103977827+Hassan0Kamel@users.noreply.github.com>
2023-05-24 22:09:31 +0300

    Create nopx.c

---
+++ nopx.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  *f_nop- nog
+  *@head: sead
+  *@counter: liber
+  *Return: nourn
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
