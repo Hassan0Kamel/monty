342b9990761c60322da798295dc788159ed5128c
Hassan0Kamel <103977827+Hassan0Kamel@users.noreply.github.com>
2023-05-24 22:16:09 +0300

    Create pstrx.c

---
+++ pstrx.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * f_pstr - prints t
+ * @head: stad
+ * @counter: linmber
+ * Return: nrn
+*/
+
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *m;
+	(void)counter;
+
+	h = *head;
+	while (m)
+	{
+		if (m->n > 127 || m->n <= 0)
+		{
+			break;
+		}
+		printf("%c", m->n);
+		m = m->next;
+	}
+	printf("\n");
+}
